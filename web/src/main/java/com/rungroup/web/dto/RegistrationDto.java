345407b5cf15b4a5e23fc8794310b66c1e24060b
SikePoop <sike20022020@gmail.com>
2023-07-09 21:18:18 +0700

    Add web

---
+++ web/src/main/java/com/rungroup/web/dto/RegistrationDto.java
@@ -0,0 +1,17 @@
+package com.rungroup.web.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+
+public class RegistrationDto {
+    private Long id;
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String password;
+}
