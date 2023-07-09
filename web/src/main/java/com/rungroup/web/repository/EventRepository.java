c80e3adadeca8948bb4c8be81ae59705a243624b
SikePoop <sike20022020@gmail.com>
2023-07-09 21:20:47 +0700

    Revese

--- web/src/main/java/com/rungroup/web/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rungroup.web.repository;
-
-import com.rungroup.web.models.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
--- web/src/main/java/com/rungroup/web/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rungroup.web.repository;
-
-import com.rungroup.web.models.Role;
-import com.rungroup.web.models.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
